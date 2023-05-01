f7ff8a84820b621368902e32b18d8ae1138021ab
DioonS <itzdiih@gmail.com>
2023-05-01 17:26:18 -0300

    Trabalhando com Servlets Tomcat

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/HelloWorld.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador.servlet;
+
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "hello", value = "/hello")
+public class HelloWorld extends HttpServlet{
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Hello World!");
+        out.println("Parabens voce escreveu o primeiro Servlet");
+        out.println("<body>");
+        out.println("<html>");
+
+        System.out.println("Carregado");
+    }
+}
