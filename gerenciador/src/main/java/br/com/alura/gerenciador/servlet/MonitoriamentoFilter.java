b7e0e8f660cea6f51861cc9511a1e99f9c07ed22
DioonS <itzdiih@gmail.com>
2023-05-05 19:21:37 -0300

    Usando filtros para mapear o tempo de execução

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/MonitoriamentoFilter.java
@@ -0,0 +1,29 @@
+package br.com.alura.gerenciador.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/entrada")
+public class MonitoriamentoFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+
+        long antes = System.currentTimeMillis();
+
+        String acao = req.getParameter("acao");
+
+        // Executar a ação
+        chain.doFilter(req, resp);
+
+        long depois = System.currentTimeMillis();
+
+        System.out.println("Tempo de execução da ação: " + acao + " -> " + (depois - antes));
+        System.out.println("Tempo antes: " + antes + " - Tempo depois: " + depois);
+    }
+
+
+}
