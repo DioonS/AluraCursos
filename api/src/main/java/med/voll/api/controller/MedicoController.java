4b3286757abfddd0ff7b7fd1c2d18906949944e1
DioonS <itzdiih@gmail.com>
2023-05-09 17:48:00 -0300

    Adicionando segunda Migration

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,12 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
+    @Transactional
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
 }
---
+++ api/src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -0,0 +1,18 @@
+CREATE TABLE medicos (
+
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    crm VARCHAR(6) NOT NULL UNIQUE,
+    especialidade VARCHAR(100) NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    complemento  VARCHAR(100),
+    numero VARCHAR(20),
+    uf VARCHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(id)
+
+);
