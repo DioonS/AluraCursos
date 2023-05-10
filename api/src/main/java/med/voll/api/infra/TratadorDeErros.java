85c3e57dc97cb1e2a0e1a4bd00a3fd5f1a3f1a81
DioonS <itzdiih@gmail.com>
2023-05-10 20:21:43 -0300

    Tratando erro 400

--- api/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,22 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(dadosErroValidacao::new).toList());
+    }
+
+    // Criar o DTO aqui mesmo sem criar um record a parte
+    private record dadosErroValidacao(
+            String campo,
+            String mensagem) {
+
+        public dadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
