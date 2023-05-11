a4c591fe94ad2689ca51dd2db045f4af26204320
DioonS <itzdiih@gmail.com>
2023-05-10 21:12:27 -0300

    Adicionando repository e service dos Usuarios

---
+++ api/src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// Serviço de Autenticação
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
